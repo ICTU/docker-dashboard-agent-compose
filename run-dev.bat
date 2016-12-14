75a74a6abd2adb26bceae37aa4f15c23d721a50b
giren <giovanni.renfurm@ictu.nl>
2016-12-14 15:46:09 +0100

    Dev script for starting agent on windows

---
+++ run-dev.bat
@@ -0,0 +1,11 @@
+set AUTH_TOKEN=123456& ^
+set HTTP_PORT=8080& ^
+set VLAN=& ^
+set MQTT_URL=mqtt://localhost& ^
+set DOMAIN=infra& ^
+set TLD=ictu& ^
+set SCRIPT_BASE_DIR=X:\mnt\data\scripts& ^
+set DATA_DIR=X:\mnt\data& ^
+set HOST_IF=eth0& ^
+set REMOTEFS_URL=http://10.25.161.250:8000& ^
+nodemon index.coffee
