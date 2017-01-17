4d5b13cd05ff287bc2bf4f987043cd8e01e16899
jeroenpeeters <jeroen@peetersweb.nl>
2017-01-17 13:43:30 +0100

    updated clojure project file

--- project.clj
+++ project.clj
@@ -9,22 +9,22 @@
   :npm {:dependencies [[source-map-support "0.4.0"]
                        [lodash "4.17.4"]]}
   :source-paths ["src/clojure" "target/classes"]
-  :clean-targets ["out" "release"]
+  ; :clean-targets ["out" "release"]
   :target-path "target"
 
   :cljsbuild
   { :builds
-    [{:id "simple"
-      :source-paths ["clojure"]
+    [{:id "nodejs"
+      :source-paths ["src/clojure"]
       :compiler
-      { :output-dir "out"
+      {
+        :output-dir "target"
         :optimizations :none
         :cache-analysis true
         :source-map true
         :verbose true
-        :output-wrapper false
         :target :nodejs
         :modules
         { :compose
-          { :output-to "out/compose/compose.js"
-            :entries #{"compose.compose"}}}}}]})
+          { :output-to "compose/compose.js"
+            :entries #{compose.compose}}}}}]})
