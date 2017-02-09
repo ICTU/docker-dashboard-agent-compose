2d45a880fa8761e5e5ce953977256efa4b074f4f
jeroenpeeters <jeroen@peetersweb.nl>
2017-02-09 11:44:16 +0100

    docker_host example config

--- run-dev.sh
+++ run-dev.sh
@@ -2,6 +2,7 @@
 
 docker run -v $PWD:/build -w /build clojure:lein-2.7.1-alpine lein cljsbuild once nodejs
 
+DOCKER_HOST=tcp://10.19.88.248:2375 \
 AUTH_TOKEN=infra \
 HTTP_PORT=8080 \
 VLAN=3080 \
