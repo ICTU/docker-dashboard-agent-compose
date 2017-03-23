7fc7a2f7e0a2c0651a2d5fcc267346a4cd3c620c
jeroenpeeters <jeroen@peetersweb.nl>
2017-03-23 10:58:21 +0100

    example of healtcheck test overrides

--- run-dev.sh
+++ run-dev.sh
@@ -2,17 +2,19 @@
 
 docker run --rm -v $PWD:/build -w /build clojure:lein-2.7.1-alpine lein cljsbuild once nodejs
 
-DOCKER_HOST=tcp://10.19.88.246:2375 \
+DOCKER_HOST=tcp://10.19.88.248:2375 \
 AUTH_TOKEN=innovation \
 HTTP_PORT=8080 \
 VLAN=3080 \
 MQTT_URL=mqtt://localhost \
 MQTT_USER=user \
 MQTT_PASS=pass \
-DOMAIN=innovation \
+DOMAIN=infra \
 TLD=ictu \
 SCRIPT_BASE_DIR=/local/data/scripts \
 DATA_DIR=/local/data \
-HOST_IF=ens192 \
-REMOTEFS_URL=http://10.19.88.246:8000 \
+HOST_IF=eth1 \
+REMOTEFS_URL=http://10.19.88.248:8000 \
+NETWORK_HEALTHCHECK_TEST_INTERFACE=eth0 \
+NETWORK_HEALTHCHECK_TEST_IP_PREFIX=10.25 \
 nodemon index.coffee
