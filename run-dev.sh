86608d6c5c07036997b6cb38d16b3760b6f8b112
jeroenpeeters <jeroen@peetersweb.nl>
2017-04-14 09:52:23 +0200

    example config

--- run-dev.sh
+++ run-dev.sh
@@ -13,6 +13,12 @@ SCRIPT_BASE_DIR=/local/data/scripts \
 DATA_DIR=/local/data \
 HOST_IF=eth1 \
 REMOTEFS_URL=http://10.19.88.248:8000 \
+NETWORK_SCAN_INTERVAL=60000 \
+NETWORK_SCAN_ENABLED=true \
+NETWORK_HEALTHCHECK_INTERVAL=2s \
+NETWORK_HEALTHCHECK_TIMEOUT=35s \
+NETWORK_HEALTHCHECK_RETRIES=10 \
+NETWORK_HEALTHCHECK_TEST="if [ ! -f /tmp/healthcheck ]; then ifconfig eth0 | grep 'inet addr:'; [ \$\$? -eq 0 ] && touch /tmp/healthcheck; else sleep 30; ifconfig eth0 | grep 'inet addr:'; fi" \
 NETWORK_HEALTHCHECK_TEST_INTERFACE=eth0 \
 NETWORK_HEALTHCHECK_TEST_IP_PREFIX=10.25 \
 nodemon index.coffee
