8b3353c9dee84dc3b26faad386420339e0abd2db
Greyarch BV <gantcho@greyarch.net>
2017-09-21 15:45:07 +0200

    initial swarm support; no unit tests

--- run-swarm-dev.sh
+++ run-swarm-dev.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-DOCKER_HOST=tcp://10.19.88.101:2375 \
-AUTH_TOKEN=agent \
+DOCKER_HOST=tcp://10.25.96.22:2375 \
+AUTH_TOKEN=innovation \
 HTTP_PORT=8080 \
 MQTT_URL=mqtt://localhost \
 MQTT_USER=user \
 MQTT_PASS=pass \
 DOMAIN=sandbox \
-TLD=ictu \
+TLD=swarm \
 SCRIPT_BASE_DIR=/local/data/scripts \
 DATA_DIR=/local/data \
 REMOTEFS_URL=http://10.19.88.248:8000 \
-NETWORK_NAME=swarm-mcv3096 \
-NETWORK_PARENT_NIC=eth0 \
+NETWORK_NAME=swarm-net \
 NETWORK_SCAN_INTERVAL=60000 \
 NETWORK_SCAN_ENABLED=false \
 NETWORK_HEALTHCHECK_INTERVAL=2s \
